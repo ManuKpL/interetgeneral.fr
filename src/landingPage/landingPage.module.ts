3679c14d59db4c6dc07eead633acbe7c722bc0ec
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2017-10-29 14:07:33 +0100

    [landing] :cop: removed unused import

--- src/landingPage/landingPage.module.ts
+++ src/landingPage/landingPage.module.ts
@@ -1,11 +1,9 @@
 import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { LandingComponent }        from './landing.component';
 import { SocialNetworksComponent } from './socialNetworks/socialNetworks.component';
 
 @NgModule({
-  imports:      [BrowserModule],
   exports:      [LandingComponent],
   declarations: [LandingComponent, SocialNetworksComponent],
   bootstrap:    [LandingComponent]
