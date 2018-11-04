25f215d765c63b315ac823a5bf9e9b9b70f5f632
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-11-04 23:20:28 +0100

    [FRONT][:wrench:] node env management > prod mode

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,20 @@
-const { environment }              = require('@rails/webpacker');
-const html                         = require('./loaders/html');
-const typescript                   = require('./loaders/typescript');
-const style                        = require('./loaders/style');
-const { join, resolve }            = require('path');
-const { ContextReplacementPlugin } = require('webpack');
+const { environment }   = require('@rails/webpacker');
+const html              = require('./loaders/html');
+const typescript        = require('./loaders/typescript');
+const style             = require('./loaders/style');
+const { join, resolve } = require('path');
+const {
+  ContextReplacementPlugin, DefinePlugin,
+} = require('webpack');
 
 const appDir = resolve(__dirname, '..', '..', 'app');
 const ContextReplacement = new ContextReplacementPlugin(
   /@angular(\\|\/)core(\\|\/)esm5/,
   join(appDir, 'javascript'),
 );
+const env = new DefinePlugin({
+  NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+});
 
 const sassLoader   = environment.loaders.get('sass');
 sassLoader.use     = style.loaders;
@@ -20,5 +25,6 @@ environment.loaders.append('html', html);
 environment.loaders.append('typescript', typescript);
 
 environment.plugins.append('ContextReplacement', ContextReplacement);
+environment.plugins.append('env', env);
 
 module.exports = environment;
