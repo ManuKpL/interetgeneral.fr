258a0c0f3dba89aea3c4b4c268f1288258145abe
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-11-05 02:32:02 +0100

    [:construction:] WIP: model json format from BFF instead of front resource service

--- app/javascript/src/landingPage/announcements.resource.ts
+++ app/javascript/src/landingPage/announcements.resource.ts
@@ -4,9 +4,6 @@ import { Observable }      from 'rxjs/Observable';
 import { of }              from 'rxjs/observable/of';
 import { map, catchError } from 'rxjs/operators';
 
-import * as moment from 'moment';
-import 'moment/locale/fr';
-
 @Injectable()
 export default class AnnouncementsResource {
 
@@ -21,27 +18,9 @@ export default class AnnouncementsResource {
   // ******************************** PRIVATE ******************************* //
 
   private getCurrentAnnouncement(): Observable<IAnnouncement | {}> {
-
-    const handleReturn = (model: IAnnouncementApiData): IAnnouncement => {
-      const dateToMoment: moment.Moment = moment(model.date).locale('fr');
-
-      if (!model) { return; }
-
-      return {
-        title: model.title,
-        day: dateToMoment.format('dddd'),
-        date: dateToMoment.format('DD MMM'),
-        hour: dateToMoment.format('LT'),
-        location: model.location,
-        address: model.address,
-        eventLink: model.event_link,
-        mapLink: model.map_link,
-      };
-    };
-
     return this.http.get(this.ANNOUNCEMENT_BASE_PATH)
       .pipe(
-        map((res: Response): IAnnouncement => handleReturn(res.json())),
+        map((res: Response): IAnnouncement => res.json() as IAnnouncement),
         catchError((e: Error) => { console.error(e); return of(e); }),
       );
   }
--- app/models/infographic.rb
+++ app/models/infographic.rb
@@ -1,3 +1,11 @@
 class Infographic < ApplicationRecord
   belongs_to :edition
+
+  def json_format
+    {
+      id:     id,
+      imgSrc: image_url,
+      title:  title,
+    }
+  end
 end
