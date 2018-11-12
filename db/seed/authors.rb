586242b62883877e2fb37bd045051fa964ec41bb
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-11-13 00:40:53 +0100

    [BFF] seed++ > articles/authors

--- db/seed/authors.rb
+++ db/seed/authors.rb
@@ -1,66 +1,79 @@
 module AuthorSeed
   @values = [
     {
+      # id: 1,
       name: 'Vigna',
       first_name: 'Xavier',
       description: 'Historien',
     },
     {
+      # id: 2,
       name: 'Bosser',
       first_name: 'Alain',
       description: 'Economiste',
     },
     {
+      # id: 3,
       name: 'Clouet',
       first_name: 'Hadrien',
       description: 'Sociologue',
     },
     {
+      # id: 4,
       name: 'Linhart',
       first_name: 'Danièle',
       description: 'Sociologue, CNRS',
     },
     {
+      # id: 5,
       name: 'Framont',
       first_name: 'Nicolas',
       description: 'Rédacteur en chef de la revue Frustration',
     },
     {
+      # id: 6,
       name: 'Poliak',
       first_name: 'Claude',
       description: 'Sociologue',
     },
     {
+      # id: 7,
       name: 'Ruffin',
       first_name: 'François',
       description: 'Rédacteur en chef du journal Fakir',
     },
     {
+      # id: 8,
       name: 'Etiévant',
       first_name: 'Guillaume',
       description: "Expert auprès des comités d'entreprise",
     },
     {
+      # id: 9,
       name: 'Webb',
       first_name: 'Sophie',
       description: 'Historienne',
     },
     {
+      # id: 10,
       name: 'Lebaron',
       first_name: 'Frédéric',
       description: 'Sociologue',
     },
     {
+      # id: 11,
       name: 'Moral Darleux',
       first_name: 'Corinne',
       description: "Secrétaire nationale du PG à l'écosocialisme, conseillère régionale Auvergne-Rhône-Alpes",
     },
     {
+      # id: 12,
       name: 'Ambrourousi',
       first_name: 'Noam',
       description: 'Haut-fonctionnaire',
     },
     {
+      # id: 13,
       name: 'Dockès',
       first_name: 'Emmanuel',
       description: 'Juriste, Groupe de Recherche - Pour un Autre Code du Travail',
