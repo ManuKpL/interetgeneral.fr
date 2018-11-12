a022785cbbbd0963ca22c993f3a96998e596c8a6
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-11-12 23:29:00 +0100

    [:hammer:] * rework seed * added author seed * added editions/:id route

---
+++ db/seed/announcements.rb
@@ -0,0 +1,26 @@
+module AnnouncementSeed
+  @values = [
+    {
+      title: "Soirée de lancement de L'Intérêt général #3",
+      date: DateTime.new(2018,5,31,19,0,0,'+2'),
+      location: 'Le Lieu Dit',
+      address: '6 rue Sorbier, 75020 PARIS',
+      event_link: 'https://www.facebook.com/events/1663258210425411/',
+      map_link: 'http://www.openstreetmap.org/node/1930249941',
+      current: true
+    },
+    {
+      title: "Soirée de lancement de L'Intérêt général #3 dans la Creuse!",
+      date: DateTime.new(2018,6, 16,18,0,0,'+2'),
+      location: 'Salle des fêtes',
+      address: 'Faux-la-Montagne',
+      event_link: 'https://www.facebook.com/events/2125444431054215/',
+      map_link: 'https://goo.gl/maps/64Z9BrtSJjt',
+      current: false
+    }
+  ]
+
+  def self.values
+    @values
+  end
+end
---
+++ db/seed/infographics.rb
@@ -0,0 +1,23 @@
+module InfographicsSeed
+  @values = [
+    {
+      image_url: '/packs/_/assets/images/infographies/IG-2-hommesFemmes-c0bb2baac89f76fbb44281037b5ef325.png',
+      title: 'IG-2-hommesFemmes',
+      edition_id: 2
+    },
+    {
+      image_url: '/packs/_/assets/images/infographies/IG-2-ouvriersRisques-d7628fcf1ee587f0dc46e8314507c811.png',
+      title: 'IG-2-ouvriersRisques',
+      edition_id: 2
+    },
+    {
+      image_url: '/packs/_/assets/images/infographies/IG-2-travailPrecarite-dd551d94a1fdc2c97ba91c19c8b9b38e.png',
+      title: 'IG-2-travailPrecarite',
+      edition_id: 2
+    }
+  ]
+
+  def self.values
+    @values
+  end
+end
