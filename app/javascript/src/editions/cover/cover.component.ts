58eff4ed8e978cf1a93937683b5ff2e02690a53b
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-11-13 00:42:54 +0100

    [FRONT] redirect to edition issue if at least one article is added

--- app/javascript/src/editions/cover/cover.component.ts
+++ app/javascript/src/editions/cover/cover.component.ts
@@ -1,17 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ig-editions-cover',
   templateUrl: './cover.template.html',
   styleUrls: ['./cover.styles.scss'],
 })
-export default class CoverComponent {
+export default class CoverComponent implements OnInit {
 
   @Input() cover: ICoverDef;
 
   public displayFull = false;
+  public redirectLink: string;
+  public redirectTarget: string;
+
+  constructor(private router: Router) {}
 
   public onImageLoad(): void {
     this.displayFull = true;
+  }
+
+  ngOnInit() {
+    if (this.cover.articlesCount) {
+      this.redirectLink = `/editions/${this.cover.editionId}`;
+      this.redirectTarget = '_self';
+    }
+    else {
+      this.redirectLink = this.cover.shopPath;
+      this.redirectTarget = '_blank';
+    }
   }
 }
