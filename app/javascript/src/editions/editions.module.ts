1b289f1ef935a7eef70fef40db89850880dd6317
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-11-17 23:19:46 +0100

    [:construction:] * :hammer: renamed edition component to issue component * :lipstick: added style for responsive editionIssue header display

--- app/javascript/src/editions/editions.module.ts
+++ app/javascript/src/editions/editions.module.ts
@@ -1,17 +1,17 @@
-import { NgModule }      from '@angular/core';
-import { CommonModule }  from '@angular/common';
-import { HttpModule }    from '@angular/http';
+import { NgModule }     from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpModule }   from '@angular/http';
 
-import CoverComponent    from './cover/cover.component';
-import EditionsComponent from './editions.parent/editions.component';
-import EditionsResource  from './editions.resource';
-import EditionComponent from './edition.issue/edition.component';
+import CoverComponent        from './cover/cover.component';
+import EditionsComponent     from './editions.parent/editions.component';
+import EditionsResource      from './editions.resource';
+import EditionIssueComponent from './issue/issue.component';
 
 @NgModule({
-  bootstrap: [EditionsComponent],
-  declarations: [CoverComponent, EditionComponent, EditionsComponent],
-  exports: [EditionsComponent],
-  imports: [CommonModule, HttpModule],
-  providers: [EditionsResource]
+  bootstrap   : [EditionsComponent],
+  declarations: [CoverComponent, EditionIssueComponent, EditionsComponent],
+  exports     : [EditionsComponent],
+  imports     : [CommonModule, HttpModule],
+  providers   : [EditionsResource]
 })
 export default class EditionsModule { }
