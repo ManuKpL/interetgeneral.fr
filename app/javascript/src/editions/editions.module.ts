0441cc75b76de62236ae21ce101e3fd78f6d03b4
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-11-20 00:12:47 +0100

    [FRONT] created basic issue article component to hold article presentation in issue page

--- app/javascript/src/editions/editions.module.ts
+++ app/javascript/src/editions/editions.module.ts
@@ -6,10 +6,11 @@ import CoverComponent        from './cover/cover.component';
 import EditionsComponent     from './editions.parent/editions.component';
 import EditionsResource      from './editions.resource';
 import EditionIssueComponent from './issue/issue.component';
+import IssueArticleComponent from './article/article.component';
 
 @NgModule({
   bootstrap   : [EditionsComponent],
-  declarations: [CoverComponent, EditionIssueComponent, EditionsComponent],
+  declarations: [CoverComponent, EditionIssueComponent, EditionsComponent, IssueArticleComponent],
   exports     : [EditionsComponent],
   imports     : [CommonModule, HttpModule],
   providers   : [EditionsResource]
