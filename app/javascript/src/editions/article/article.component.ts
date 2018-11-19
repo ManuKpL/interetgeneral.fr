c183ec6545862f9f8c449ebad9a53ca9b78d749f
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2018-11-20 00:12:47 +0100

    [FRONT] created basic issue article component to hold article presentation in issue page

---
+++ app/javascript/src/editions/article/article.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'ig-issue-article',
+  templateUrl: './article.template.html',
+  styleUrls: ['./article.styles.scss'],
+})
+export default class IssueArticleComponent {
+  @Input() article: IIssueArticle;
+  constructor() {}
+}
