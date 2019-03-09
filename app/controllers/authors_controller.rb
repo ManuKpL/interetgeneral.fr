38308b66f4100ea2c6e181311006f507f81c1bb5
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-03-09 11:19:49 +0100

    [API] :hammer: controllers > refacto for status 204

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,16 +1,23 @@
 class AuthorsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :sample]
-  before_action :get_authors, only: :index
-  before_action :get_authors_sample, only: :sample
+  before_action      :get_authors,        only: :index
+  before_action      :get_authors_sample, only: :sample
 
   def index
-    render json: @authors
+    if @authors.empty?
+      render status: :no_content
+    else
+      render status: :ok, json: @authors
+    end
   end
 
   private
 
   def get_authors
-    @authors = Edition.find(editions_params[:edition_id]).authors.order(name: :asc)
+    @authors = Edition
+      .find(editions_params[:edition_id])
+      .authors
+      .order(name: :asc)
   end
 
   def editions_params
--- app/controllers/infographics_controller.rb
+++ app/controllers/infographics_controller.rb
@@ -3,16 +3,23 @@ class InfographicsController < ApplicationController
   before_action :get_infographics, only: :index
 
   def index
-    render json: @infographics.map(&:json_format)
+    if @infographics.empty?
+      render status: :no_content
+    else
+      render status: :ok, json: @infographics.map(&:to_home_json_format)
+    end
   end
 
   private
 
   def get_infographics
     if (infographics_params.has_key?(:limit))
-      @infographics = Infographic.order(created_at: :desc).limit(infographics_params[:limit])
+      @infographics = Infographic
+        .order(created_at: :desc)
+        .limit(infographics_params[:limit])
     else
-      @infographics = Infographic.order(created_at: :desc)
+      @infographics = Infographic
+        .order(created_at: :desc)
     end
   end
 
