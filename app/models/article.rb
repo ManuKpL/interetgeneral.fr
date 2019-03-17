f2c1e54b4d401842209fc9f81e0225d15d62e8c3
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-03-17 18:01:05 +0100

    [API] articles controller / model / route management

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,7 +9,28 @@ class Article < ApplicationRecord
       :id     => id,
       :type   => article_type,
       :title  => title,
-      :author => author.to_json_issue_format,
+      :author => author.to_json_simple_format,
+    }
+  end
+
+  def to_json_simple_format
+    {
+      :id => id,
+      :type => article_type,
+      :title => title,
+      :lead => lead,
+      :author => author.to_json_simple_format
+    }
+  end
+
+  def to_json_details_format
+    {
+      :id      => id,
+      :type    => article_type,
+      :title   => title,
+      :lead    => lead,
+      :content => content,
+      :author  => author.to_json_simple_format
     }
   end
 end
