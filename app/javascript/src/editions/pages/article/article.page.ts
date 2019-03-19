2621c671022bc5763cb704015223960e69676038
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-03-20 00:44:46 +0100

    [APP] article page: get article details from API

--- app/javascript/src/editions/pages/article/article.page.ts
+++ app/javascript/src/editions/pages/article/article.page.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { switchMap, map } from 'rxjs/operators';
+import { Observable, throwError, from, empty } from 'rxjs';
+import { switchMap, catchError, switchMapTo } from 'rxjs/operators';
+import { EditionsResource } from '../../services';
+
+const validatesRegex = (regex: RegExp) => (s: string) => regex.test(s);
+const splitShift     = (split: string | RegExp) => (s: string) => s.split(split).shift();
 
 @Component({
   selector   : 'ig-editions-article',
@@ -10,23 +14,54 @@ import { switchMap, map } from 'rxjs/operators';
 })
 export class ArticlePage implements OnInit {
 
+  public article$: Observable<any>;
+
   public constructor(
-    private router: Router,
-    private route: ActivatedRoute,
+    private resource: EditionsResource,
+    private route:    ActivatedRoute,
+    private router:   Router,
   ) {}
 
-  private static readonly ID_FORMAT    = /^\d+(-[a-z]+)+/i;
-  private static readonly PARAMS_NAMES = ['editionId', 'articleId'];
+  private readonly ID_FORMAT         = /^\d+(-[a-z]+)+/i;
+  private readonly PARAMS_NAMES      = ['editionId', 'articleId'];
+  private readonly INVALID_IDS_ERROR = 'INVALID_IDS';
 
   public ngOnInit(): void {
-    const ids$ = this._readParamIds(this.route.paramMap);
+    this.article$ = this.route.paramMap
+      .pipe(
+        switchMap(this._readParamIds()),
+        catchError(this._handleError()),
+      );
   }
 
   // PRIVATE METHOD --------------------------------------------------------------------------------
 
-  private _readParamIds(params$: Observable<ParamMap>): Observable<string[]> {
-    return params$.pipe(
-      map(params => ArticlePage.PARAMS_NAMES.map(name => params.get(name))),
-    );
+  private _readParamIds(): (params: ParamMap) => Observable<string | string[]> {
+    return (params: ParamMap) => {
+      const ids = this.PARAMS_NAMES.map(name => params.get(name));
+      return this._parseParamIds(ids);
+    };
+  }
+
+  private _parseParamIds(ids: string[]): Observable<string | string[]> {
+    if (ids.every(validatesRegex(this.ID_FORMAT))) {
+      return this._getArticle(ids.map(splitShift('-')) as [string, string]);
+    }
+    return throwError(this.INVALID_IDS_ERROR);
+  }
+
+  private _getArticle(ids: [string, string]): Observable<any> {
+    return this.resource.getEditionArticle(...ids);
+  }
+
+  private _handleError(): (error: any) => Observable<never> {
+    return (error: any) => {
+      if (error === this.INVALID_IDS_ERROR) {
+        console.error('Invalid ids in path params');
+      }
+
+      return from(this.router.navigate(['/']))
+        .pipe(switchMapTo(empty()));
+    };
   }
 }
