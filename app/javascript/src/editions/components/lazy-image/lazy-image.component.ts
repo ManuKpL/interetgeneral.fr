e598f45e24dd468476f6e5e9005e4c09291c9fd7
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-03-26 23:40:31 +0100

    [APP] editions module > added new lazy image component to handle image load

---
+++ app/javascript/src/editions/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector   : 'ig-lazy-img',
+  styleUrls  : ['./lazy-image.component.scss'],
+  templateUrl: './lazy-image.component.html',
+})
+export class NameComponent implements OnInit {
+
+  public classNames: { img: string, preview: string };
+  public imageDisplayFull = false;
+
+  @Input()
+  public illustration: IIllustration;
+
+  @Input('classNames')
+  private _classNames = '';
+
+  public ngOnInit(): void {
+    this.classNames = {
+      img    : this._classNames,
+      preview: [this._classNames, 'lazy-preview'].join(' '),
+    };
+  }
+
+  public onImageLoad(): void {
+    this.imageDisplayFull = true;
+  }
+}
