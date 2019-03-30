c6b666c1edfc7280b49545e858c8b345e31d5e2e
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-03-31 00:03:56 +0100

    [APP] added pipe for html & style sanitization

--- app/javascript/src/editions/editions.module.ts
+++ app/javascript/src/editions/editions.module.ts
@@ -5,13 +5,15 @@ import { HttpModule }   from '@angular/http';
 import * as components from './components';
 import * as pages      from './pages';
 import * as services   from './services';
+import * as pipes      from './pipes';
 
 const componentsList = Object.values(components);
 const pagesList      = Object.values(pages);
+const pipesList      = Object.values(pipes);
 const servicesList   = Object.values(services);
 
 @NgModule({
-  declarations: [...pagesList, ...componentsList],
+  declarations: [...pagesList, ...componentsList, ...pipesList],
   exports     : [...pagesList],
   imports     : [CommonModule, HttpModule],
   providers   : [...servicesList]
