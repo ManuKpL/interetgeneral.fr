a3b784a9880d47fe14b514085e4771d537342e3b
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-03-31 23:34:46 +0200

    [API] merge existing infographics with new article/illustration couple

--- app/controllers/infographics_controller.rb
+++ app/controllers/infographics_controller.rb
@@ -1,26 +1,32 @@
 class InfographicsController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
-  before_action :get_infographics, only: :index
+  skip_before_action :authenticate_user!,   @@only_index
+  before_action :get_infographics,          @@only_index
+  before_action :get_articles_infographics, @@only_index
 
   def index
-    if @infographics.empty?
+    if @infographics.empty? && @articles.empty?
       render_no_content
-    else
-      render status: :ok, json: @infographics.map(&:to_home_json_format)
+      return
     end
+
+    infographics = @infographics + @articles
+
+    if infographics_params.has_key?(:limit)
+      infographics = infographics.slice(0, 3)
+    end
+
+    render status: :ok, json: infographics.map(&:to_home_json_format)
   end
 
   private
 
   def get_infographics
-    if (infographics_params.has_key?(:limit))
-      @infographics = Infographic
-        .order(created_at: :desc)
-        .limit(infographics_params[:limit])
-    else
-      @infographics = Infographic
-        .order(created_at: :desc)
-    end
+    @infographics = Infographic.order(created_at: :desc)
+  end
+
+  def get_articles_infographics
+    @articles = Article.where(:article_type => Article.article_types[:INFOGRAPHIC])
+      .order(:created_at => :desc)
   end
 
   def infographics_params
