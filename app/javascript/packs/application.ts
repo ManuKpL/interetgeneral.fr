9e17aa42c1d1580d0d81c243d145687776016f35
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-04-07 19:06:02 +0200

    [API] added new images for editions & missing admin form fields

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -1,7 +1,9 @@
+require('../../assets/images/covers/4-europe.png');
 require('../../assets/images/covers/3-campagnes.png');
 require('../../assets/images/covers/2-travail.png');
 require('../../assets/images/covers/1-medias.png');
 
+require('../../assets/images/covers/preview/4-europe.png');
 require('../../assets/images/covers/preview/3-campagnes.png');
 require('../../assets/images/covers/preview/2-travail.png');
 require('../../assets/images/covers/preview/1-medias.png');
