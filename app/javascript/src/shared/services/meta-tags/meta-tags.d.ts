46f77f42295aa26178da4d2717158c251b855516
Emmanuel Capelle <emmanuel.capelle@gmail.com>
2019-05-08 20:43:50 +0200

    [APP] added shared module and created meta tags service

---
+++ app/javascript/src/shared/services/meta-tags/meta-tags.d.ts
@@ -0,0 +1,14 @@
+declare type TwitterMetaTagsValues = {
+  card:        string,
+  title:       string,
+  description: string,
+  url:         string,
+  image:       string,
+};
+
+declare type OpenGraphMetaTagsValues = {
+  title:       string,
+  description: string,
+  url:         string,
+  image:       string,
+}
